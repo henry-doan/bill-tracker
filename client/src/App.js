e43e4ea2c2a917fef7c06385136f6da59ce2fa2d
Henry Doan <henrydoan96@gmail.com>
2024-06-23 17:13:47 -0600

    added working auth

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import Home from './components/dashboard/Home';
+import NoMatch from './components/shared/NoMatch';
+import Navbar from './components/shared/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <>
+    <Navbar />
+    <>
+      <FetchUser>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<NoMatch />} />
+        </Routes>
+      </FetchUser>
+    </>
+  </>
+)
+
+export default App;
---
+++ client/src/components/dashboard/Home.js
@@ -0,0 +1,5 @@
+const Home = () => (
+  <h3>Home Dashboard</h3>
+)
+
+export default Home;
